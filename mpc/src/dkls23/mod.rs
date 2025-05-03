fb1ce138aa22e37483e252431c1b72b6c8ef4198
David Tran <tranhuyducseven@gmail.com>
2025-05-03 10:57:57 +0700

    feat/mpc: layout dkls23

---
+++ mpc/src/dkls23/mod.rs
@@ -0,0 +1,16 @@
+mod hash;
+mod key_gen;
+mod setup;
+mod sign;
+mod verify;
+mod wallet;
+
+// This is a simple implementation or usage test of the DKLs23 signature scheme.
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_hello() {
+        println!("Hello, world!");
+    }
+}
--- mpc/src/lib.rs
+++ mpc/src/lib.rs
@@ -1,3 +1,5 @@
+mod dkls23;
 mod simple;
 
+pub use dkls23::*;
 pub use simple::*;
